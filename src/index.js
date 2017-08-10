1bbe69e1f2cb0ae528034080bda4514dd6cd2af8
Karla Hugo <karla.hugo@granicus.com>
2017-08-10 16:16:58 -0500

    component folder structure youtube api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyBpYco08kEyRC4XDU0N4mHC_L1KY6UB0FU'
+
 // Create a new component. This component should produce some HTML.
 const App = () => {
   return <div> Hi! </div>; //JSX
