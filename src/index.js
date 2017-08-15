2d5337e629c6b78ceee6091a0e2b14f5e93dc61c
Karla Hugo <karla.hugo@granicus.com>
2017-08-15 10:26:52 -0500

    started styling

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class App extends Component {
   }
   render(){
     return(
-      <div>
+      <div className="main-container">
         <SearchBar />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
